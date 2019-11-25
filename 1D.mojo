056e76878905363f7065698e1bcd0e6093ef0e24
Jeremy Chew <jc1356@berkeley.edu>
2019-11-25 17:26:38 +0800

    added code from google docs

--- 1D.mojo
+++ 1D.mojo
@@ -1,19 +1,27 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <project name="1D" board="Mojo V3" language="Verilog">
   <files>
+    <src>tile_led.luc</src>
     <src>alu.luc</src>
+    <src>tile.luc</src>
     <src top="true">mojo_top.v</src>
+    <src>led_flasher.luc</src>
     <src>square.v</src>
     <src>cmp16.luc</src>
     <src>alu_tester.luc</src>
     <src>manual_tester.luc</src>
     <src>multi_seven_seg.luc</src>
     <src>adder16.luc</src>
+    <src>tiles.luc</src>
+    <src>tile_bitmap.v</src>
     <src>vga.v</src>
+    <src>state_counter.luc</src>
     <src>auto_tester.luc</src>
     <src>bool16.luc</src>
     <src>muldiv16.luc</src>
+    <src>game_loop.luc</src>
     <src>seven_seg.luc</src>
+    <src>game_controller.luc</src>
     <src>shift16.luc</src>
     <ucf lib="true">io_shield.ucf</ucf>
     <ucf>mojo.ucf</ucf>
--- source/mojo_top.v
+++ source/mojo_top.v
@@ -20,16 +20,16 @@ module mojo_top (
     input avr_rx_busy,      // AVR RX buffer full
     
     output [23:0] io_led,   // LEDs on IO Shield
-    output [7:0] io_seg,      // 7-segment LEDs on IO Shield
-    output [3:0] io_sel,      // Digit select on IO Shield
+//    output [7:0] io_seg,      // 7-segment LEDs on IO Shield
+//    output [3:0] io_sel,      // Digit select on IO Shield
     input [5:0] io_button,    // 5 buttons on IO Shield
-    input [23:0] io_dip,     // DIP switches on IO Shield
+    input [23:0] io_dip     // DIP switches on IO Shield
     
-    output r,
-    output g,
-    output b,
-    output vs,
-    output hs
+    //output r,
+    //output g,
+    //output b,
+    //output vs,
+    //output hs
   );
 
   wire rst = ~rst_n; // make reset active high
@@ -40,105 +40,148 @@ module mojo_top (
   assign spi_channel = 4'bzzzz;
   
   assign led = 8'b0;
+  //assign io_led[23:1] = 23'b0;
   
-  assign r = rval;
-  assign g = gval;
-  assign b = bval;
-  assign vs = vsync;
-  assign hs = hsync;
+  //assign r = rval;
+  //assign g = gval;
+  //assign b = bval;
+  //assign vs = vsync;
+  //assign hs = hsync;
   
-  assign io_seg = iseg;
-  assign io_sel = isel;
-  assign io_led = ioled;
+//  assign io_seg = iseg;
+//  assign io_sel = isel;
+//  assign io_led = ioled;
   
   wire [9:0] x;
   wire [9:0] y;
   
-  wire [7:0] iseg;
-  wire [3:0] isel;
+  wire [27:0] tiles_g;
+  wire [27:0] tiles_p;
   
-  wire [23:0] ioled;
+//  wire [7:0] iseg;
+//  wire [3:0] isel;
   
-  wire [9:0] sq1_xl, sq1_xr, sq1_yt, sq1_yb;
-  wire [9:0] sq2_xl, sq2_xr, sq2_yt, sq2_yb;
-  wire [9:0] sq3_xl, sq3_xr, sq3_yt, sq3_yb;
+//  wire [23:0] ioled;
+  
+//  wire [9:0] sq1_xl, sq1_xr, sq1_yt, sq1_yb;
+//  wire [9:0] sq2_xl, sq2_xr, sq2_yt, sq2_yb;
+//  wire [9:0] sq3_xl, sq3_xr, sq3_yt, sq3_yb;
     
-  vga vga (
-    .clk(clk),
-    .rst(rst),
-    .x(x[9:0]),
-    .y(y[9:0]),
-    .valid(valid),
-    .hsync(hsync),
-    .vsync(vsync),
-    .newframe(newframe),
-    .newline(newline),
-    .pixclk(pixclk)
-  );
+//  vga vga (
+//    .clk(clk),
+//    .rst(rst),
+//    .x(x[9:0]),
+//    .y(y[9:0]),
+//    .valid(valid),
+//    .hsync(hsync),
+//    .vsync(vsync),
+//    .newframe(newframe),
+//    .newline(newline),
+//    .pixclk(pixclk)
+//  );
   
-  square #(.IX(10'd160), .IY(10'd120), .H_SIZE(10'd60), .X_DIR(1), .Y_DIR(1)) square1 (
-    .clk(clk),
-    .rst(rst),
-    .pixclk(pixclk),
-    .animate(newframe),
-    .o_xl(sq1_xl),
-    .o_xr(sq1_xr),
-    .o_yt(sq1_yt),
-    .o_yb(sq1_yb)
-  );
+//  square #(.IX(10'd160), .IY(10'd120), .H_SIZE(10'd60), .X_DIR(1), .Y_DIR(1)) square1 (
+//    .clk(clk),
+//    .rst(rst),
+//    .pixclk(pixclk),
+//    .animate(newframe),
+//    .o_xl(sq1_xl),
+//    .o_xr(sq1_xr),
+//    .o_yt(sq1_yt),
+//    .o_yb(sq1_yb)
+//  );
   
-  square #(.IX(10'd400), .IY(10'd240), .H_SIZE(10'd80), .X_DIR(0), .Y_DIR(1)) square2 (
-    .clk(clk),
-    .rst(rst),
-    .pixclk(pixclk),
-    .animate(newframe),
-    .o_xl(sq2_xl),
-    .o_xr(sq2_xr),
-    .o_yt(sq2_yt),
-    .o_yb(sq2_yb)
-  );
+//  square #(.IX(10'd400), .IY(10'd240), .H_SIZE(10'd80), .X_DIR(0), .Y_DIR(1)) square2 (
+//    .clk(clk),
+//    .rst(rst),
+//    .pixclk(pixclk),
+//    .animate(newframe),
+//    .o_xl(sq2_xl),
+//    .o_xr(sq2_xr),
+//    .o_yt(sq2_yt),
+//    .o_yb(sq2_yb)
+//  );
   
-  square #(.IX(10'd300), .IY(10'd320), .H_SIZE(10'd40), .X_DIR(1), .Y_DIR(0)) square3 (
-    .clk(clk),
-    .rst(rst),
-    .pixclk(pixclk),
-    .animate(newframe),
-    .o_xl(sq3_xl),
-    .o_xr(sq3_xr),
-    .o_yt(sq3_yt),
-    .o_yb(sq3_yb)
-  );
-  
-  alu_tester tester (
-    .clk(clk),
-    .rst(rst),
-    .auto_sel(io_button[2]), // down button
-    .auto_next(io_button[1]), // middle button
-    .man_sel(io_button[0]), // up button
-    .man_next(io_button[4]), // right button
-    .man_out(io_button[3]), // left button
-    .switches(io_dip[23:0]),
-    .seg(iseg[7:0]),
-    .sel(isel[3:0]),
-    .leds(ioled[23:0])
-  );
+//  square #(.IX(10'd300), .IY(10'd320), .H_SIZE(10'd40), .X_DIR(1), .Y_DIR(0)) square3 (
+//    .clk(clk),
+//    .rst(rst),
+//    .pixclk(pixclk),
+//    .animate(newframe),
+//    .o_xl(sq3_xl),
+//    .o_xr(sq3_xr),
+//    .o_yt(sq3_yt),
+//    .o_yb(sq3_yb)
+//  );
 
-  reg rval, bval, gval;
+//  alu_tester tester (
+//    .clk(clk),
+//    .rst(rst),
+//    .auto_sel(io_button[2]), // down button
+//    .auto_next(io_button[1]), // middle button
+//    .man_sel(io_button[0]), // up button
+//    .man_next(io_button[4]), // right button
+//    .man_out(io_button[3]), // left button
+//    .switches(io_dip[23:0]),
+//    .seg(iseg[7:0]),
+//    .sel(isel[3:0]),
+//    .leds(ioled[23:0])
+//  );
+
+    game_loop gl (
+      .clk(clk),
+      .rst(rst),
+      .up(io_button[0]),
+      .down(io_button[2]),
+      .left(io_button[3]),
+      .right(io_button[4]),
+      .tiles_g(tiles_g[27:0]),
+      .tiles_p(tiles_p[27:0])
+    );
+        
+    assign io_led[20:0] = tiles_g[20:0];
+    assign io_led[23:21] = 2'b000;
+
+//  tiles tiles (
+//    .clk(clk),
+//    .rst(rst),
+//    .x(x[9:0]),
+//    .y(y[9:0]),
+//    .up(io_button[0]),
+//    .down(io_button[2]),
+//    .left(io_button[3]),
+//    .right(io_button[4]),
+//    .tile1(tile1),
+//    .tile2(tile2),
+//    .tile3(tile3),
+//    .tile4(tile4),
+//    .tile5(tile5),
+//    .tile6(tile6),
+//    .tile7(tile7),
+//    .tile8(tile8),
+//    .tile9(tile9)
+//  );
+  
+  //reg rval, bval, gval;
   
   always @(*) begin
   
-    rval = 0;
-    gval = 0;
-    bval = 0;
+    //rval = 0;
+    //gval = 0;
+    //bval = 0;
     
-    if (valid) begin
+    //if (valid) begin
       //rval = (x < 320);
       //bval = (x < 160 || (x < 480 && x > 320));
       //gval = (x < 80 || (x < 240 && x > 160) || (x < 400 && x > 320) || (x < 560 && x > 480));
-      rval = ((x > sq1_xl) & (x < sq1_xr) & (y > sq1_yt) & (y < sq1_yb));
-      bval = ((x > sq2_xl) & (x < sq2_xr) & (y > sq2_yt) & (y < sq2_yb));
-      gval = ((x > sq3_xl) & (x < sq3_xr) & (y > sq3_yt) & (y < sq3_yb));
-    end
+      //rval = ((x > sq1_xl) & (x < sq1_xr) & (y > sq1_yt) & (y < sq1_yb));
+      //bval = ((x > sq2_xl) & (x < sq2_xr) & (y > sq2_yt) & (y < sq2_yb));
+      //gval = ((x > sq3_xl) & (x < sq3_xr) & (y > sq3_yt) & (y < sq3_yb));
+      
+      //rval = tile1 | tile2 | tile3 | tile4 | tile5 | tile6 | tile7 | tile8 | tile9;
+      //gval = tile1 | tile2 | tile3 | tile4 | tile5 | tile6 | tile7 | tile8 | tile9;
+      //bval = tile1 | tile2 | tile3 | tile4 | tile5 | tile6 | tile7 | tile8 | tile9;
+      
+    //end
   end
   
 endmodule
